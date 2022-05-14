adca810681c0a871f24273b2912f13e672e2b15c
mhoffnn <matheushofmar@hotmail.com>
2022-05-13 23:19:31 -0300

    Top secret

--- top-secret/lib/top_secret.ex
+++ top-secret/lib/top_secret.ex
@@ -3,7 +3,8 @@ defmodule TopSecret do
     Code.string_to_quoted!(string)
   end
 
-	def decode_secret_message_part({operation, _, args} = ast, acc) when operation in [:def, :defp] do
+  def decode_secret_message_part({operation, _, args} = ast, acc)
+      when operation in [:def, :defp] do
     {f_name, f_arity} = get_f_name_arity(args)
     {ast, [String.slice(to_string(f_name), 0, f_arity) | acc]}
   end
@@ -23,6 +24,7 @@ defmodule TopSecret do
   def decode_secret_message(string) do
     ast = to_ast(string)
     {_, acc} = Macro.prewalk(ast, [], &decode_secret_message_part/2)
+
     acc
     |> Enum.reverse()
     |> Enum.join("")
