039bce288e6cb14663d4d950ff6c592138cf3138
mhoffnn <matheushofmar@hotmail.com>
2022-05-16 15:01:47 -0300

    Newsletter

--- newsletter/lib/newsletter.ex
+++ newsletter/lib/newsletter.ex
@@ -1,21 +1,41 @@
 defmodule Newsletter do
   def read_emails(path) do
-    # Please implement the read_emails/1 function
+    path
+    |> File.read!()
+    |> String.split()
   end
 
   def open_log(path) do
-    # Please implement the open_log/1 function
+    path
+    |> File.open!([:write])
   end
 
   def log_sent_email(pid, email) do
-    # Please implement the log_sent_email/2 function
+    pid
+    |> IO.puts(email)
   end
 
   def close_log(pid) do
-    # Please implement the close_log/1 function
+    pid
+    |> File.close()
   end
 
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    pid = open_log(log_path)
+
+    emails_path
+    |> read_emails()
+    |> send_mail(pid, send_fun)
+
+    close_log(pid)
   end
+
+  defp send_mail([], _, _), do: nil
+  defp send_mail([mail|tail], pid, send_fun) do
+    if send_fun.(mail) == :ok do
+      log_sent_email(pid, mail)
+    end
+    send_mail(tail, pid, send_fun)
+  end
+
 end
